3e2fbf81c63268e0041a401828b36fb09bdbfe34
Cristian Prieto <pcristiandev@gmail.com>
2025-05-03 12:37:13 -0300

    Agregue la capa dominio con las clases
    Se utilizara un enfoque de Arquitectura en capas para separar el
    dominio. Las clases que se agregan son Pokemon y Elemento.

---
+++ PokemonManager.dominio/Pokemon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonManager.dominio
+{
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        public int Numero { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string UrlImagen { get; set; }
+        public bool EstaActivo { get; set; }
+        public Elemento Tipo { get; set; }
+        public Elemento Debilidad { get; set; }
+    }
+}
