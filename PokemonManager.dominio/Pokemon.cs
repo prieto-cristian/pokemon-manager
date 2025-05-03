0e64ac912e9f3d6da4f106cffe5d837b15bf7e99
Cristian Prieto <pcristiandev@gmail.com>
2025-05-03 13:44:32 -0300

    Modifique el nombre de las columnas
    Se modifica el nombre de las columnas a nivel de disenio de clases.

--- PokemonManager.dominio/Pokemon.cs
+++ PokemonManager.dominio/Pokemon.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +10,13 @@ namespace PokemonManager.dominio
     public class Pokemon
     {
         public int Id { get; set; }
+        [DisplayName("Número")]
         public int Numero { get; set; }
         public string Nombre { get; set; }
+        [DisplayName("Descripción")]
         public string Descripcion { get; set; }
         public string UrlImagen { get; set; }
+        [DisplayName("Activo")]
         public bool EstaActivo { get; set; }
         public Elemento Tipo { get; set; }
         public Elemento Debilidad { get; set; }
